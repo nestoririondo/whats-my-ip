5d5beb597cfe23639f0ea294b7b0d2a25d6e421d
Néstor Iriondo <nestoririondo@gmail.com>
2024-01-12 16:50:58 +0100

    country data fetch is inside the ip data fetch

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -23,31 +23,30 @@ function Map() {
   const [isLoading, setIsLoading] = useState(true);
 
   const ipifyApiKey = import.meta.env.VITE_REACT_APP_IPIFY_API_KEY;
-    // const countrylayerApiKey = import.meta.env
-    //   .VITE_REACT_APP_COUNTRYLAYER_API_KEY;
 
-  const getMyLocation = async () => {
+  const getData = async () => {
     try {
       const response = await axios.get(
         `https://geo.ipify.org/api/v2/country,city?apiKey=${ipifyApiKey}`
       );
       setMyLocation(response.data);
+      try {
+        const countryResponse = await axios.get(
+          `https://restcountries.com/v3.1/alpha/${response.data.location.country}`
+        );
+        setLocationData(countryResponse.data);
+      } catch {
+        console.log("Error fetching country data");
+      }
     } catch {
-      console.log("Error");
-    }
-  };
-
-  const getLocationData = async () => {
-    try {
-      const response = await axios.get(`https://restcountries.com/v3.1/all`);
-      setLocationData(response.data);
-    } catch {
-      console.log("Error");
+      console.log("Error fetching location data");
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    Promise.all([getMyLocation(), getLocationData()]).then(() => setIsLoading(false));
+    getData();
   }, []);
 
   if (isLoading) {
@@ -59,11 +58,7 @@ function Map() {
     location: { country, region, lat, lng },
   } = myLocation;
 
-  const countryData = locationData.filter((thisCountry) => {
-    return thisCountry.cca2 === country;
-  });
-
-  const { capital, name, flag, coatOfArms, timezones } = countryData[0];
+  const { capital, name, flag, coatOfArms, timezones } = locationData[0];
 
   const today = DateTime.now().setZone(timezones[0]).toFormat("MM-dd-yyyy");
   const now = DateTime.now().setZone(timezones[0]).toFormat("HH:mm");
@@ -75,7 +70,7 @@ function Map() {
     >
       <div className="app">
         <div className="map">
-          <MapContainer center={[lat, lng]} zoom={13} scrollWheelZoom={true}>
+          <MapContainer center={[lat, lng]} zoom={10} scrollWheelZoom={true}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
